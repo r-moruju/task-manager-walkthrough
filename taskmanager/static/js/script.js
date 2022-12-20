3e84f59562537d94ed05462dd4c10b30a86767c3
R Moruju <morujurazvan@gmail.com>
2022-12-20 21:41:00 +0000

    Implement add task feature

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,4 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmm, yyyy",
+        i18n: {done: "Select"}
+    });
+
+    let select = document.querySelectorAll('select');
+    M.FormSelect.init(select);
 });
